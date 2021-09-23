e9d02df72692b834ff45f232da2b78745109df21
이유성 <82145837+meteor-or@users.noreply.github.com>
2021-09-24 07:52:24 +0900

    second commit~~~~~`

--- asset/src/main.js
+++ asset/src/main.js
@@ -23,30 +23,7 @@ AOS.init({
 
 // Chagne product img 
 
-let promotion = {
-  itme1: {
-    name: '헬리콥터 추격전',
-    price: '44,900',
-    img: 'product1.png'
-  },
-  item2: {
-    name: '경찰서',
-    price: '129,000',
-    img: 'product2.png'
-  },
-  item3: {
-    name: '헬리콥터 운송 트럭',
-    price: '64,900',
-    img: 'product3.png'
-  }
-}
-console.log(promotion.item2.img);
-$('.legoTitle').on('click',function(){
-  $('img[alt=promotion]').attr({src:'asset/img/' + promotion.item2.img});
-  $('.priceTxt').text(promotion.item2.price);
-  $('.subTitle').text(promotion.item2.name);
-})
-
+// 애니메이션페이지
 
 $('.animationItem').on({
   'mouseenter': function(){
@@ -58,7 +35,7 @@ $('.animationItem').on({
 });
 
 // 유튜브 플러그인
-$(function(){// 제이쿼리 문서 준비 이벤트
+$(function(){
   $("#youtube").YTPlayer();
 });
 
@@ -68,10 +45,10 @@ $(function(){// 제이쿼리 문서 준비 이벤트
 function colorChange() {  
   let movez = document.querySelectorAll(".bg-changer");  
   for (let i = 0; i < movez.length; i++) {   
-    var start = this.scrollY - movez[i].offsetTop + movez[i].clientHeight / 3;
-    var stop = this.scrollY - movez[i].offsetTop - movez[i].clientHeight;  
-    var rate = movez[i].getAttribute('data-bgcolor');
-    var html = document.getElementsByTagName("html")[0];
+    let start = this.scrollY - movez[i].offsetTop + movez[i].clientHeight / 3;
+    let stop = this.scrollY - movez[i].offsetTop - movez[i].clientHeight;  
+    let rate = movez[i].getAttribute('data-bgcolor');
+    let html = document.getElementsByTagName("html")[0];
     if ( start > 0 && stop < 0 ){ 
       document.body.style.backgroundColor = rate; 
     }   
@@ -79,7 +56,21 @@ function colorChange() {
 } 
 requestAnimationFrame(colorChange);
 
-//드래그 색상 변경
+// 일반 상품 스크롤 이벤트
+
+  window.addEventListener('scroll', function(){
+    let currentScrollValue = this.scrollY
+    let productScroll = document.querySelector('.productWrap').offsetTop*3 + document.querySelector('.productWrap').clientHeight  
+    if ( currentScrollValue  >=  productScroll) {
+      this.document.querySelector('.productList').classList.add('active')
+    } else if ( currentScrollValue  <  productScroll){
+      this.document.querySelector('.productList').classList.remove('active')
+    }
+    
+  });
+
+
+  //드래그 색상 변경
 
 let r = 0; 
 const loop = () => { 
@@ -93,6 +84,7 @@ const loop = () => {
 //
 
 // kakao api
+// 검색
 $('.searchBtn').click(function(){
   $.ajax({
     method: "GET",
@@ -101,10 +93,49 @@ $('.searchBtn').click(function(){
     headers: {Authorization : "KakaoAK 17194e8da000acd54324eeff27f5ca14"}
   })
     .done(function (msg) {
-      console.log(msg.documents[0].title);
-      console.log(msg.documents[0].contents);
       $('.searchTitle').append(msg.documents[0].title);
       $('.searchContents').append(msg.documents[0].contents);
       $('.searchUrl').append(msg.documents[0].url);
     });
+});
+
+//물품 이미지 가져오기
+  // $.ajax({
+  //   method: "GET",
+  //   url: "https://dapi.kakao.com/v2/search/image",
+  //   data: { query :  '레고 헬리콥터 운송트럭' },
+  //   headers: {Authorization : "KakaoAK 17194e8da000acd54324eeff27f5ca14"}
+  // })
+  //   .done(function (msg) {
+  //     console.log(msg.documents[4].image_url);
+  //     $('.productImgBox>img').attr({src:'msg.documents[4].image_url'});
+  //   });
+
+// 돌아가는 텍스트
+
+$.each($(".ezkorry-roller"), function(index, item) {
+
+  const wrapper = $("<div />", { class:"ezkorry-roller-wrapper"});
+  const roller = $(item);
+  roller.append(wrapper);
+  const span = roller.find('span').first();
+  wrapper.append(span);
+  const span_width = span.get(0).offsetWidth;
+  const max_width = roller.width() + span_width;
+  let inner_width = span_width;
+
+  while(max_width > inner_width) {
+  wrapper.append(span.clone());
+  inner_width += span_width;
+  }
+
+  anime({
+  targets: wrapper.get(0),
+  translateX: {
+    value: '-=' + span_width + 'px',
+    duration: 6000
+  },
+  loop: true,
+  easing: 'linear'
+  });
 });
